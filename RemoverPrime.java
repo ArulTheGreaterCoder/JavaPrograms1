7297a685b652885583b7e0af67b418c5a8da27ac
ArulTheGreaterCoder <arulkansal04@gmail.com>
2026-08-23 12:20:23 +0530

    14

---
+++ RemoverPrime.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class RemoverPrime{
+    public static void solution(ArrayList<Integer> al){
+        for(int i = al.size() - 1; i>=0; i--){
+            if(isPrime(al.get(i)) == true){
+                al.remove(i);
+            }
+        }
+    }
+    public static boolean isPrime(int n){
+        if (n<=1){
+            return false;
+        }
+        for(int c = 2; c<=n; c++){
+            if(n%c==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number of integers you want to input: ");
+        int n = scan.nextInt();
+
+        ArrayList<Integer> al = new ArrayList<>();
+
+        System.out.println("Enter the "+n+" integers:");
+        for(int i = 0; i<n; i++){
+            al.add(scan.nextInt());
+        }
+        solution(al);
+
+        System.out.println("Modified list (without primes): "+al);
+    }
+}
