ead2f3203ef28a24e2298246237084c8a1e80c72
ArulTheGreaterCoder <arulkansal04@gmail.com>
2026-08-30 12:26:08 +0530

    14

---
+++ ReverseList.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class ReverseList{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the list: ");
+        int n = scn.nextInt();
+
+        ArrayList<Integer> al = new ArrayList<>();
+        System.out.println("Enter the "+n+" integers:");
+        for(int i = 0; i<n; i++){
+            al.add(scn.nextInt());
+        }
+        ArrayList<Integer> ans = reverse(al);
+
+        System.out.println("Reversed List: "+ans);
+    }
+    static ArrayList<Integer> reverse(ArrayList<Integer> list){
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = list.size()-1; i>=0; i--){
+            ans.add(list.get(i));
+        }
+        return ans;
+    }
+}
