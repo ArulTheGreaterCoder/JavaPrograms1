060ea06b901437f0d094e7492ca0ce8fae1faf78
ArulTheGreaterCoder <arulkansal04@gmail.com>
2026-09-13 12:20:35 +0530

    14

---
+++ DisplayArrayrev.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DisplayArrayrev{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the "+n+ " elements of the array:");
+        for(int i = 0; i<n; i++){
+            arr[i] = scn.nextInt();
+        }
+        displayArrRev(arr,0);
+    }
+    public static void displayArrRev(int[] arr, int idx){
+        if(idx == arr.length){
+            return;
+        }
+        displayArrRev(arr, idx+1);
+        System.out.println(arr[idx]);
+        
+    }
+}
